984c8517d4bba67e3d799f3331429a929f8d5724
Pierre Khoury <pierrekhouryb@gmail.com>
2017-02-12 20:21:13 -0800

    Implemented lung segmentation with 2 methods: zuidhof and arnavjain. The first is more precise, but the second seems more robust.

--- src/imageprocessing.py
+++ src/imageprocessing.py
@@ -4,8 +4,12 @@ import dicom
 import os
 import scipy.ndimage
 import matplotlib.pyplot as plt
-from skimage import measure, morphology
+from skimage import data, filters, measure, morphology, feature, segmentation
 
+# LUNA 16 constants
+MIN_BOUND = -1000.0
+MAX_BOUND = 400.0
+PIXEL_MEAN = 0.25
 
 # Select n patients from folder, based on method.
 # Folder must be containing patients folders only.
@@ -49,7 +53,7 @@ def select_patients_by_index(indices, folder):
 
     return [patients[i] for i in indices]
 
-# Load single patient
+# Load single patient.
 def load_scan(patient_folder):
     slices = [dicom.read_file(patient_folder + '/' + s) for s in os.listdir(patient_folder)]
     slices.sort(key = lambda x: int(x.ImagePositionPatient[2]))
@@ -87,7 +91,7 @@ def load_scan(patient_folder):
 
     return (get_pixels_hu(slices), slices)
 
-# Preprocess single patient
+# Preprocess single patient.
 def preprocess_scan(image, scan, do_resample=True, do_normalize=True, do_zerocenter=True):
     if not (do_resample or do_normalize or do_zerocenter):
         return image
@@ -108,23 +112,120 @@ def preprocess_scan(image, scan, do_resample=True, do_normalize=True, do_zerocen
         return image #, new_spacing
 
     def normalize(image):
-        MIN_BOUND = -1000.0
-        MAX_BOUND = 400.0
         image = (image - MIN_BOUND) / (MAX_BOUND - MIN_BOUND)
         image[image>1] = 1.
         image[image<0] = 0.
         return image
 
     def zerocenter(image):
-        PIXEL_MEAN = 0.25
         image = image - PIXEL_MEAN
         return image
 
     if do_resample:
         preprocessed = resample(image, scan)
     if do_normalize:
-        preprocessed = normalize(image)
+        preprocessed = normalize(preprocessed)
     if do_zerocenter:
-        preprocessed = zerocenter(image)
+        preprocessed = zerocenter(preprocessed)
 
     return preprocessed
+
+# Extract lungs from 3d scan data based on method.
+def extract_lungs_in_scan(in_image, return_mask=False, method='zuidhof'):
+    # make sure trheshold is based on same range
+    # add switch for method
+    # add switch for binary mask/real data
+
+    def extract_lungs_in_scan_arnavjain(in_image):
+        threshold = (-400 - MIN_BOUND) / (MAX_BOUND - MIN_BOUND) - PIXEL_MEAN
+        def segment_slice(im):
+            binary = im < threshold
+            cleared = segmentation.clear_border(binary)
+            label_image = measure.label(cleared)
+
+            areas = [r.area for r in measure.regionprops(label_image)]
+            areas.sort()
+            if len(areas) > 2:
+                for region in measure.regionprops(label_image):
+                    if region.area < areas[-2]:
+                        for coordinates in region.coords:
+                               label_image[coordinates[0], coordinates[1]] = 0
+            binary = label_image > 0
+            selem = morphology.disk(2)
+            binary =  morphology.binary_erosion(binary, selem)
+            selem =  morphology.disk(10)
+            binary =  morphology.binary_closing(binary, selem)
+
+            edges = filters.roberts(binary)
+            binary = scipy.ndimage.binary_fill_holes(edges)
+
+            return binary
+
+        out_mask = np.zeros(in_image.shape)
+        for i in range(in_image.shape[0]):
+            out_mask[i,...] = segment_slice(in_image[i,...])
+
+        return out_mask
+
+    def extract_lungs_in_scan_zuidhof(in_image, fill_lung_structures=True):
+        def largest_label_volume(im, bg=-1):
+            vals, counts = np.unique(im, return_counts=True)
+
+            counts = counts[vals != bg]
+            vals = vals[vals != bg]
+
+            if len(counts) > 0:
+                return vals[np.argmax(counts)]
+            else:
+                return None
+        # not actually binary, but 1 and 2.
+        # 0 is treated as background, which we do not want
+        threshold = (-320 - MIN_BOUND) / (MAX_BOUND - MIN_BOUND) - PIXEL_MEAN
+        binary_image = np.array(in_image > threshold, dtype=np.int8)+1
+        labels = measure.label(binary_image)
+
+        # Pick the pixel in the very corner to determine which label is air.
+        #   Improvement: Pick multiple background labels from around the patient
+        #   More resistant to "trays" on which the patient lays cutting the air
+        #   around the person in half
+        background_label = labels[0,0,0]
+
+        #Fill the air around the person
+        binary_image[background_label == labels] = 2
+
+
+        # Method of filling the lung structures (that is superior to something like
+        # morphological closing)
+        if fill_lung_structures:
+            # For every slice we determine the largest solid structure
+            for i, axial_slice in enumerate(binary_image):
+                axial_slice = axial_slice - 1
+                labeling = measure.label(axial_slice)
+                l_max = largest_label_volume(labeling, bg=0)
+
+                if l_max is not None: #This slice contains some lung
+                    binary_image[i][labeling != l_max] = 1
+
+
+        binary_image -= 1 #Make the image actual binary
+        binary_image = 1-binary_image # Invert it, lungs are now 1
+
+        # Remove other air pockets insided body
+        labels = measure.label(binary_image, background=0)
+        l_max = largest_label_volume(labels, bg=0)
+        if l_max is not None: # There are air pockets
+            binary_image[labels != l_max] = 0
+
+        return binary_image
+
+    if method=='arnavjain':
+        seg_method = extract_lungs_in_scan_arnavjain
+    elif method=='zuidhof':
+        seg_method = extract_lungs_in_scan_zuidhof
+
+    mask = seg_method(in_image)
+
+    if return_mask:
+        return mask
+    else:
+        return in_image*mask
--- src/script_pk.py
+++ src/script_pk.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 import imageprocessing
 
-def savefig(fig, filename, savedir="./fig/"):
+def savefig(fig, filename, savedir="../fig/"):
     if not os.path.exists(savedir):
         os.makedirs(savedir)
     fig.savefig(savedir+filename,bbox_inches='tight')
@@ -11,23 +11,35 @@ def check_image(image, title='', save=False):
     fig = plt.figure(num=None, figsize=(16,12.8), dpi=80, facecolor='w', edgecolor='k')
     plt.hist(image.flatten(), bins=128, color='c')
     plt.title(title)
-    plt.xlim((-1000, 2500))
+    # plt.xlim((-1000, 2500))
     plt.ticklabel_format(style='sci',scilimits=(0,1),axis='y')
     if save:
         savefig(fig, title)
-    else:
-        plt.draw()
+
+def disp_image(image, sliceindex, title='', save=False):
+    fig = plt.figure(num=None, figsize=(16,12.8), dpi=80, facecolor='w', edgecolor='k')
+    plt.imshow(image[sliceindex,:,:], cmap=plt.cm.bone)
+    plt.colorbar()
+    if save:
+        savefig(fig, title)
 
 # INPUT_FOLDER = 'Y:\\dsb2017\\sample_images\\'
 # list_patients = os.listdir(INPUT_FOLDER)
 # list_patients.sort()
-
+do_save = True
 INPUT_FOLDER = 'Y:\\dsb2017\\stage1\\'
+list_patients = imageprocessing.select_patients(15, INPUT_FOLDER, 'first')
+for i, p in enumerate(list_patients):
+    image, scan = imageprocessing.load_scan(INPUT_FOLDER+p)
+    image = imageprocessing.preprocess_scan(image, scan)
+    check_image(image,'hist_' + p , save=do_save)
+    disp_image(image, image.shape[0]/2, str(p)+'_orig_s'+str(image.shape[0]/2), save=do_save)
+    bimage = imageprocessing.extract_lungs_in_scan(image, return_mask=False, method='arnavjain')
+    check_image(bimage,'hist_seg_arnavjain_' + p, save=do_save)
+    disp_image(bimage, bimage.shape[0]/2, str(p)+'_arnavjain_s'+str(bimage.shape[0]/2), save=do_save)
+    bimage = imageprocessing.extract_lungs_in_scan(image, return_mask=False, method='zuidhof')
+    check_image(bimage,'hist_seg_zuidhof_' + p, save=do_save)
+    disp_image(bimage, bimage.shape[0]/2, str(p)+'_zuidhof_s'+str(bimage.shape[0]/2), save=do_save)
+    print(i)
 
-list_patients = imageprocessing.select_patients(3, INPUT_FOLDER, 'first')
-# list_patients = dataio.select_patients_by_index([0, 1, 4, 6], INPUT_FOLDER)
-
-# for i, p in enumerate(list_patients):
-#     image, scan = dataio.load_scan(INPUT_FOLDER+patients[i])
-#     image = dataio.preprocess_scan(image, scan)
-#     check_image(image,patients[i], save=True)
+# plt.show()

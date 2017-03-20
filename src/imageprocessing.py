f88b298c357bb42e1643caa138ba9d2006dc5536
Pierre Khoury <pierrekhouryb@gmail.com>
2017-03-19 17:08:09 -0700

    Refactored data loading methods to support LUNA16 dataset. Moved plotting methods to imageprocessing module.

--- src/imageprocessing.py
+++ src/imageprocessing.py
@@ -12,21 +12,101 @@ import scipy.ndimage
 import matplotlib.pyplot as plt
 import random
 from skimage import data, filters, measure, morphology, feature, segmentation
+import SimpleITK as sitk
 
 logger = logging.getLogger(__name__)
+
+# Just a couple of utility functions
+def savefig(fig, filename, savedir="../fig/"):
+    savedir = os.path.abspath(savedir)
+    if not os.path.exists(savedir):
+        logger.warning('Out dir not existing, creating {}'.format(savedir))
+        os.makedirs(savedir)
+    logger.info(' - save image to: {}'.format(savedir))
+    fig.savefig(os.path.join(savedir, filename + '.png'),
+                bbox_inches='tight', format='png')
+
+
+def check_image(image, title='', save=False, output_dir=None):
+    fig = plt.figure(num=None, figsize=(16, 12.8), dpi=80,
+        facecolor='w', edgecolor='k')
+    plt.hist(image.flatten(), bins=128, color='c')
+    plt.title(title)
+    # plt.xlim((-1000, 2500))
+    plt.ticklabel_format(style='sci', scilimits=(0, 1), axis='y')
+    if save:
+        savefig(fig, title, savedir=output_dir)
+    else:
+        plt.show()
+
+
+def disp_image(image, sliceindex, title='', save=False, output_dir=None):
+    if isinstance(sliceindex, float):
+        logger.warning(
+            'sliceindex is a float: {}, rounding to: {}'.format(
+                sliceindex, int(sliceindex)))
+        sliceindex = int(sliceindex)
+    fig = plt.figure(num=None, figsize=(16, 12.8), dpi=80,
+                     facecolor='w', edgecolor='k')
+    plt.imshow(image[sliceindex, :, :], cmap=plt.cm.bone)
+    plt.colorbar()
+    if save:
+        savefig(fig, title, savedir=output_dir)
+    else:
+        plt.show()
+
+
+def disp_image_3axis(image, zf, xf, yf, title='', with_stats=False,
+                     save=False, output_dir=None):
+    (Z, X, Y) = image.shape
+    (z, x, y) = map(lambda dim, frac: round(dim * frac), (Z, X, Y), (zf, xf, yf))
+    fig, ax = plt.subplots(1, 3, num=None, figsize=(16, 12.8), dpi=80,
+                           facecolor='w', edgecolor='k')
+    ax.ravel()[0].imshow(image[z, :, :], cmap=plt.cm.bone)
+    ax[0].plot([y, y], [0, X], 'g-')
+    ax[0].plot([0, Y], [x, x], 'g-')
+    ax[0].set_xlim([0, X])
+    ax[0].set_ylim([0, Y])
+    ax[0].set_title('z=' + str(z))
+    ax[0].set_xlabel('x')
+    ax[0].set_ylabel('y')
+    ax[1].imshow(image[:, x, :], cmap=plt.cm.bone)
+    ax[1].plot([y, y], [0, Z], 'g-')
+    ax[1].plot([0, Y], [z, z], 'g-')
+    ax[1].set_xlim([0, Y])
+    ax[1].set_ylim([0, Z])
+    ax[1].set_title('x=' + str(x))
+    ax[1].set_xlabel('y')
+    ax[1].set_ylabel('z')
+    ax[2].imshow(image[:, :, y], cmap=plt.cm.bone)
+    ax[2].plot([x, x], [0, Z], 'g-')
+    ax[2].plot([0, X], [z, z], 'g-')
+    ax[2].set_xlim([0, X])
+    ax[2].set_ylim([0, Z])
+    ax[2].set_title('y=' + str(y))
+    ax[2].set_xlabel('x')
+    ax[2].set_ylabel('z')
+    if save:
+        savefig(fig, title, savedir=output_dir)
+    else:
+        plt.show()
+
 
 # LUNA 16 constants
 MIN_BOUND = -1000.0
 MAX_BOUND = 400.0
 PIXEL_MEAN = 0.25
 
-# Select n patients from folder, based on method.
-# Folder must be containing patients folders only.
-
 
 def select_patients(n, folder, method='first'):
+    """
+    Select n patients from folder, based on method.
+    Folder must be containing patients folders/files only.
+    """
     patients = sorted(os.listdir(folder))
-    # patients = [os.path.join(folder, p) for p in patients]
+    if not os.path.isdir(os.path.join(folder, patients[0])):
+        #if the dataset is not dsb2017 but LUNA16, we only list the mhd files as patients
+        patients = [p for p in patients if os.path.splitext(p)[-1] == '.mhd']
 
     if not patients:
         logger.warning('No patients found.')
@@ -56,12 +136,16 @@ def select_patients(n, folder, method='first'):
             n -= 1
         return out_l
 
-# Select patients from folder based on their index.
-# Folder must be containing patients folders only.
-
 
 def select_patients_by_index(indices, folder):
+    """
+    Select patients from folder based on their index.
+    Folder must be containing patients folders only.
+    """
     patients = sorted(os.listdir(folder))
+    if not os.path.isdir(os.path.join(folder, patients[0])):
+        #if the dataset is not dsb2017 but LUNA16, we only list the mhd files as patients
+        patients = [p for p in patients if os.path.splitext(p)[-1] == '.mhd']
 
     if not patients:
         print('No patients found.')
@@ -76,67 +160,91 @@ def select_patients_by_index(indices, folder):
 
     return [patients[i] for i in indices]
 
-# Load single patient.
 
+def load_scan(patient_folder_or_file):
+    """
+    Load single patient.
+    Returns the CT scan in Hounsfield Units as a 3D numpy array with (Z,X,Y),
+    a dictionary with few meta info and the raw data.
+    """
+    def load_dicom(patient_folder):
+        metainfo = {}
+        raw_dicom = [dicom.read_file(
+            os.path.abspath(os.path.join(patient_folder, s))
+        ) for s in os.listdir(patient_folder)]
+        raw_dicom.sort(key=lambda x: int(x.ImagePositionPatient[2]))
+        try:
+            slice_thickness = np.abs(
+                raw_dicom[0].ImagePositionPatient[2] -
+                raw_dicom[1].ImagePositionPatient[2])
+        except:
+            slice_thickness = np.abs(
+                raw_dicom[0].SliceLocation -
+                raw_dicom[1].SliceLocation)
 
-def load_scan(patient_folder):
-    slices = [dicom.read_file(
-        os.path.abspath(os.path.join(patient_folder, s))
-    ) for s in os.listdir(patient_folder)]
-    slices.sort(key=lambda x: int(x.ImagePositionPatient[2]))
-    try:
-        slice_thickness = np.abs(
-            slices[0].ImagePositionPatient[2] -
-            slices[1].ImagePositionPatient[2])
-    except:
-        slice_thickness = np.abs(
-            slices[0].SliceLocation -
-            slices[1].SliceLocation)
+        for s in raw_dicom:
+            s.SliceThickness = slice_thickness
 
-    for s in slices:
-        s.SliceThickness = slice_thickness
+        metainfo["spacing"] = np.array(list(map(float, ([raw_dicom[0].SliceThickness] + raw_dicom[0].PixelSpacing))))
 
-    def get_pixels_hu(slices):
-        image = np.stack([s.pixel_array for s in slices])
-        # Convert to int16 (from sometimes int16),
-        # should be possible as values should always be low enough (<32k)
-        image = image.astype(np.int16)
+        def get_pixels_hu(raw_dicom):
+            image = np.stack([s.pixel_array for s in raw_dicom])
+            # Convert to int16 (from sometimes int16),
+            # should be possible as values should always be low enough (<32k)
+            image = image.astype(np.int16)
 
-        # Set outside-of-scan pixels to 0
-        # The intercept is usually -1024, so air is approximately 0
-        image[image == -2000] = 0
+            # Set outside-of-scan pixels to 0
+            # The intercept is usually -1024, so air is approximately 0
+            image[image == -2000] = 0
 
-        # Convert to Hounsfield units (HU)
-        for slice_number in range(len(slices)):
+            # Convert to Hounsfield units (HU)
+            for slice_number in range(len(raw_dicom)):
 
-            intercept = slices[slice_number].RescaleIntercept
-            slope = slices[slice_number].RescaleSlope
+                intercept = raw_dicom[slice_number].RescaleIntercept
+                slope = raw_dicom[slice_number].RescaleSlope
 
-            if slope != 1:
-                image[slice_number] = slope * \
-                    image[slice_number].astype(np.float64)
-                image[slice_number] = image[slice_number].astype(np.int16)
+                if slope != 1:
+                    image[slice_number] = slope * \
+                        image[slice_number].astype(np.float64)
+                    image[slice_number] = image[slice_number].astype(np.int16)
 
-            image[slice_number] += np.int16(intercept)
+                image[slice_number] += np.int16(intercept)
 
-        return np.array(image, dtype=np.int16)
+            return np.array(image, dtype=np.int16)
 
-    return (get_pixels_hu(slices), slices)
+        return (get_pixels_hu(raw_dicom), metainfo, raw_dicom)
 
-# Preprocess single patient.
+    def load_mhd(patient_file):
+        # Reads the image using SimpleITK
+        raw_itk = sitk.ReadImage(patient_file)
+        image = sitk.GetArrayFromImage(raw_itk)
 
+        # Not sure why, background pixels are set to -3200 in the LUNA16
+        # datasets. Setting intercept to -1024 instead to match dsb2017.
+        image[image < -1024] = -1024
 
-def preprocess_scan(image, scan, do_resample=True, do_normalize=True,
+        metainfo = {}
+        metainfo["spacing"] = np.array(list(reversed(raw_itk.GetSpacing())))
+        metainfo["origin"] = np.array(list(reversed(raw_itk.GetOrigin())))
+
+        return (image, metainfo, raw_itk)
+
+    # Naive check if input is a dicom folder or an mhd file.
+    if(os.path.isdir(patient_folder_or_file)):
+        return load_dicom(patient_folder=patient_folder_or_file)
+    else:
+        return load_mhd(patient_file=patient_folder_or_file)
+
+def preprocess_scan(image, metainfo, do_resample=True, do_normalize=True,
         do_zerocenter=True):
+    """
+    Preprocess single patient.
+    """
     logger.info(' - Preprocess the scan')
     if not (do_resample or do_normalize or do_zerocenter):
         return image
 
-    def resample(image, scan, new_spacing=[1, 1, 1]):
-        # Determine current pixel spacing
-        spacing = map(float, ([scan[0].SliceThickness] + scan[0].PixelSpacing))
-        spacing = np.array(list(spacing))
-
+    def resample(image, spacing, new_spacing=[1, 1, 1]):
         resize_factor = spacing / new_spacing
         new_real_shape = image.shape * resize_factor
         new_shape = np.round(new_real_shape)
@@ -146,7 +254,7 @@ def preprocess_scan(image, scan, do_resample=True, do_normalize=True,
         image = scipy.ndimage.interpolation.zoom(
             image, real_resize_factor, mode='nearest')
 
-        return image  # , new_spacing
+        return image, new_spacing
 
     def normalize(image):
         image = (image - MIN_BOUND) / (MAX_BOUND - MIN_BOUND)
@@ -159,7 +267,8 @@ def preprocess_scan(image, scan, do_resample=True, do_normalize=True,
         return image
 
     if do_resample:
-        preprocessed = resample(image, scan)
+        preprocessed, new_spacing = resample(image, metainfo["spacing"])
+        metainfo["new_spacing"] = new_spacing
     if do_normalize:
         preprocessed = image if not do_resample else preprocessed
         preprocessed = normalize(preprocessed)
@@ -168,12 +277,13 @@ def preprocess_scan(image, scan, do_resample=True, do_normalize=True,
             not do_resample and not do_normalize) else preprocessed
         preprocessed = zerocenter(preprocessed)
 
-    return preprocessed
-
-# Extract lungs from 3d scan data based on method.
+    return preprocessed, metainfo
 
 
 def extract_lungs_in_scan(in_image, return_mask=False, method='arnavjain'):
+    """
+    Extract lungs from 3d scan data based on method.
+    """
     logger.info(' - extract lungs in scan ({})'.format(method))
 
     def extract_lungs_in_scan_arnavjain(in_image):
--- src/script_pk.py
+++ src/script_pk.py
@@ -12,82 +12,6 @@ import argparse
 logger = logging.getLogger(__name__)
 
 
-# Just a couple of utility functions
-def savefig(fig, filename, savedir="../fig/"):
-    savedir = os.path.abspath(savedir)
-    if not os.path.exists(savedir):
-        logger.warning('Out dir not existing, creating {}'.format(savedir))
-        os.makedirs(savedir)
-    logger.info(' - save image to: {}'.format(savedir))
-    fig.savefig(os.path.join(savedir, filename + '.png'),
-                bbox_inches='tight', format='png')
-
-
-def check_image(image, title='', save=False, output_dir=None):
-    fig = plt.figure(num=None, figsize=(16, 12.8), dpi=80,
-        facecolor='w', edgecolor='k')
-    plt.hist(image.flatten(), bins=128, color='c')
-    plt.title(title)
-    # plt.xlim((-1000, 2500))
-    plt.ticklabel_format(style='sci', scilimits=(0, 1), axis='y')
-    if save:
-        savefig(fig, title, savedir=output_dir)
-    else:
-        plt.show()
-
-
-def disp_image(image, sliceindex, title='', save=False, output_dir=None):
-    if isinstance(sliceindex, float):
-        logger.warning(
-            'sliceindex is a float: {}, rounding to: {}'.format(
-                sliceindex, int(sliceindex)))
-        sliceindex = int(sliceindex)
-    fig = plt.figure(num=None, figsize=(16, 12.8), dpi=80,
-                     facecolor='w', edgecolor='k')
-    plt.imshow(image[sliceindex, :, :], cmap=plt.cm.bone)
-    plt.colorbar()
-    if save:
-        savefig(fig, title, savedir=output_dir)
-    else:
-        plt.show()
-
-
-def disp_image_3axis(image, zf, xf, yf, title='', with_stats=False,
-                     save=False):
-    (Z, X, Y) = image.shape
-    (z, x, y) = map(lambda dim, frac: round(dim * frac), (Z, X, Y), (zf, xf, yf))
-    fig, ax = plt.subplots(1, 3, num=None, figsize=(16, 12.8), dpi=80,
-                           facecolor='w', edgecolor='k')
-    ax.ravel()[0].imshow(image[z, :, :], cmap=plt.cm.bone)
-    ax[0].plot([y, y], [0, X], 'g-')
-    ax[0].plot([0, Y], [x, x], 'g-')
-    ax[0].set_xlim([0, X])
-    ax[0].set_ylim([0, Y])
-    ax[0].set_title('z=' + str(z))
-    ax[0].set_xlabel('x')
-    ax[0].set_ylabel('y')
-    ax[1].imshow(image[:, x, :], cmap=plt.cm.bone)
-    ax[1].plot([y, y], [0, Z], 'g-')
-    ax[1].plot([0, Y], [z, z], 'g-')
-    ax[1].set_xlim([0, Y])
-    ax[1].set_ylim([0, Z])
-    ax[1].set_title('x=' + str(x))
-    ax[1].set_xlabel('y')
-    ax[1].set_ylabel('z')
-    ax[2].imshow(image[:, :, y], cmap=plt.cm.bone)
-    ax[2].plot([x, x], [0, Z], 'g-')
-    ax[2].plot([0, X], [z, z], 'g-')
-    ax[2].set_xlim([0, X])
-    ax[2].set_ylim([0, Z])
-    ax[2].set_title('y=' + str(y))
-    ax[2].set_xlabel('x')
-    ax[2].set_ylabel('z')
-    if save:
-        savefig(fig, title, savedir=OUTPUT_FOLDER)
-    else:
-        plt.show()
-
-
 def parse_arguments():
     p = argparse.ArgumentParser()
     p.add_argument('-i', '--input_dir', help="input directory")
@@ -110,6 +34,7 @@ def parse_arguments():
             raise ValueError('{} is not a valid input path.'.format(in_dir))
 
         out_dir = os.path.abspath(args.output_dir)
+        logger.info('output dir set to' + out_dir)
         if not os.path.isdir(out_dir):
             raise ValueError('{} is not a valid output path.'.format(out_dir))
 
@@ -136,29 +61,36 @@ def main():
     INPUT_FOLDER, OUTPUT_FOLDER, do_save = parse_arguments()
 
     # Select a subset of patient
-    list_patients = imageprocessing.select_patients(1, INPUT_FOLDER, 'first')
+    list_patients = imageprocessing.select_patients(15, INPUT_FOLDER, 'random')
     for i, p in enumerate(list_patients):
         logger.info('Load patient {}/{} ({})'.format(i + 1, len(list_patients),
                                                      p))
 
-        image, scan = imageprocessing.load_scan(os.path.join(INPUT_FOLDER, p))
+        image, meta, scan = imageprocessing.load_scan(os.path.join(INPUT_FOLDER, p))
+        imageprocessing.check_image(image, str(p) + '_hist_orig', save=do_save,
+                        output_dir=OUTPUT_FOLDER)
         # Preprocess the scan
-        image = imageprocessing.preprocess_scan(
-            image, scan, do_resample=False, do_normalize=True, do_zerocenter=True)
+        image, meta = imageprocessing.preprocess_scan(
+            image, meta, do_resample=True, do_normalize=True, do_zerocenter=True)
         # Check and display
         # check_image(image, 'hist_' + p, save=do_save, output_dir=OUTPUT_FOLDER)
         # disp_image(image, image.shape[0] / 2,
         #           str(p) + '_orig_s' + str(image.shape[0] / 2), save=do_save)
         # Extract lungs ala arnavjain
+        imageprocessing.disp_image_3axis(image, 0.5, 0.5, 0.25,
+                    str(p) + '_preprocessed_s' + str(int(image.shape[0] / 2)),
+                    save=do_save, output_dir=OUTPUT_FOLDER)
         bimage = imageprocessing.extract_lungs_in_scan(
             image, return_mask=False, method='arnavjain')
         # Check and display
-        check_image(bimage, 'hist_seg_arnavjain_' + p, save=do_save,
+        imageprocessing.check_image(bimage, str(p) + '_hist_seg_arnavjain', save=do_save,
                     output_dir=OUTPUT_FOLDER)
-        # disp_image(bimage, bimage.shape[0] / 2,
-        #            str(p) + '_arnavjain_s' + str(bimage.shape[0] / 2),
-        #            save=do_save)
-        disp_image_3axis(bimage, 0.5, 0.5, 0.25)
+        imageprocessing.disp_image(bimage, bimage.shape[0] / 2,
+                   str(p) + '_arnavjain_s' + str(int(bimage.shape[0] / 2)),
+                   save=do_save, output_dir=OUTPUT_FOLDER)
+        imageprocessing.disp_image_3axis(bimage, 0.5, 0.5, 0.25,
+                    str(p) + '_arnavjain_s' + str(int(bimage.shape[0] / 2)),
+                    save=do_save, output_dir=OUTPUT_FOLDER)
 
         logger.info('Processing patient {} done'.format(i + 1))
 
